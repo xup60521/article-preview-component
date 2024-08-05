880fe0bb1f4954efa849e4df75b2225d3c40f538
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-05 19:23:19 +0800

    finish

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.getElementById("share-btn")?.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleMenuOpen();
+});
+
+document.getElementById("share-btn2")?.addEventListener("click", (e) => {
+    e.stopPropagation();
+    toggleMenuOpen();
+});
+
+const menu = document.getElementById("share-menu");
+document.getElementById("share-menu")?.addEventListener("click", e => e.stopPropagation())
+function toggleMenuOpen() {
+    if (!menu) return;
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+
+        gsap.fromTo(
+            menu,
+            { opacity: 0, y: 25 },
+            { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" }
+        );
+    } else {
+        gsap.to(menu, {
+            opacity: 0,
+            y: 25,
+            duration: 0.5,
+            ease: "power2.in",
+            onComplete: () => {
+                menu.style.display = "none";
+            },
+        });
+    }
+}
+
+window.addEventListener("click", () => {
+    if (menu?.style.display === "none" || menu?.style.display === "") return;
+    toggleMenuOpen();
+});
